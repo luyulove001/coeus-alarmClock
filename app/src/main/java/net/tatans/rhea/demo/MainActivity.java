282a1ce3c89f2a2d8f4a503c0f3546a9a505559d
shengxiadeyu <Mac15985798735>
2016-05-24 11:14:33 +0800

    first commit

---
+++ app/src/main/java/net/tatans/rhea/demo/MainActivity.java
@@ -0,0 +1,34 @@
+package net.tatans.rhea.demo;
+
+import android.content.Intent;
+import android.widget.TextView;
+
+import net.tatans.coeus.network.tools.BaseActivity;
+import net.tatans.rhea.demo.cache.CacheMainActivity;
+import net.tatans.rhea.network.event.OnClick;
+import net.tatans.rhea.network.view.ContentView;
+import net.tatans.rhea.network.view.ViewIoc;
+
+@ContentView(R.layout.main)
+public class MainActivity extends BaseActivity {
+
+    @OnClick(R.id.bt_global)
+    public void btnGlobal(){
+        TatansStartActivity(GlobalActivity.class);
+    }
+
+    @OnClick(R.id.bt_single)
+    public void btnSingle(){
+        TatansStartActivity(SingleActivity.class);
+    }
+
+    @OnClick(R.id.bt_tiris)
+    public void btTiris(){
+        TatansStartActivity(TestRirisActivity.class);
+    }
+    @OnClick(R.id.bt_cache)
+    public void btCache(){
+        TatansStartActivity(CacheMainActivity.class);
+    }
+
+}
